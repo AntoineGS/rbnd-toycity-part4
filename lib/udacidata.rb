6313271f2b254275336cbf6b74484f5b462937e0
AntoineGS <antoineg.simard@gmail.com>
2016-06-15 19:22:02 -0400

    Add save method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,31 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  def self.create(attributes = nil)
+  # If the object's data is already in the database
+  # create the object
+  # return the object
+  product = self.new(attributes)
+  save(product)
+  product
+  # If the object's data is not in the database
+  # create the object
+  # save the data in the database
+  # return the object
+  end
+
+  def self.save(product)
+    CSV.open(@@data_path, "a+") do |csv|
+      csv << [product.id, product.brand, product.name, product.price]
+    end
+  end
+
+  def self.all
+    CSV.read(@@data_path).each do |line|
+      puts line
+    end
+  end
 end
