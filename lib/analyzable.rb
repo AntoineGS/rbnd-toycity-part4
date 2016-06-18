7c6010914193baffba6ce0f42c4745d7813fd857
AntoineGS <antoineg.simard@gmail.com>
2016-06-17 22:59:24 -0400

    add average_price method

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,11 @@
 module Analyzable
+  def average_price(products)
+    sum = 0
+    products.each{|product| sum += product.price.to_f}
+    (sum/products.length).round(2)
+  end
 
+  def print_report(products)
+
+  end
 end
