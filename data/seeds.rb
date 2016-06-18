64a90627d6dfce10e252e9901099bb9c68a25ad7
AntoineGS <antoineg.simard@gmail.com>
2016-06-18 15:26:17 -0400

    changed seeds from 20 to 10 times

--- data/seeds.rb
+++ data/seeds.rb
@@ -6,7 +6,7 @@ require 'faker'
 def db_seed
   @data_path = File.dirname(__FILE__) + "/../data/data.csv"
   CSV.open(@data_path, "a+") do |csv|
-    20.times do |index|
+    10.times do |index|
       csv << [index+1, Faker::Company.name, Faker::Commerce.product_name, Faker::Commerce.price]
     end
   end
